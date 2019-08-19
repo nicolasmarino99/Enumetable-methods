1e4e1a28ac5cbe0aee26988d886671e9239ce453
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-08-19 18:48:19 -0500

    added my_select Method

--- enumerable-methods.rb
+++ enumerable-methods.rb
@@ -9,7 +9,17 @@ module Counting_methods
   def my_each_with_index
     (0...size).my_each do |i|
         yield self[i], i
-      end 
+    end 
+  end
+
+  def my_select
+    arr = []
+    my_each do |i|
+      if yield i
+        arr.push(i)
+      end
     end
+  arr
+  end
 
 end
