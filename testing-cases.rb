77366e30e6aee5590ea7223bbc76928e9f541ee7
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-08-19 18:53:16 -0500

    added test cases

---
+++ testing-cases.rb
@@ -0,0 +1,19 @@
+include Counting_methods
+
+#[0,2,5,6].my_each {|i| puts i}
+
+#[0,2,5,6].my_each_with_index {|i, index| puts "#{index}, #{i}" + "\n"}
+
+#[0,2,5,6].my_select {|i| i > 2}
+
+#[0,2,5,6].my_all? {|i| i >= 0}
+
+#[0,2,5,6].my_any? {|i| i >= 7}
+
+#[0,2,5,6].my_none? {|i| i >= 0}
+
+#[5,2,5,6].my_count {|i| i == 5}
+
+#[5,2,5,6].my_map {|i| i*5}
+
+#[5,2,5,6].my_inject {|accumulator, i| accumulator + i}
